d754db2bdadb104746b3a9dce14d92b1ae0a86dd
queng <quentin.brejoin@gmail.com>
2023-02-04 14:47:18 +0100

    feat(parallax): add spade go up and down

--- parallax.lua
+++ parallax.lua
@@ -1,19 +1,50 @@
 parallax = {}
 
-function parallax.load()
+local scale = 0.25
 
-    screen_height = love.graphics.getHeight()
-    randomNumber = love.math.random(1, 9)
-    sprite = love.graphics.newImage("assets/rock.png")
-    x = love.graphics.getWidth()
-    y = math.random(0, 500)
+function parallax.load()
+    sprite = love.graphics.newImage("assets/pelle.png")
+    x = -255
+    y = love.graphics.getHeight() - sprite:getHeight() * scale - 800
     speed = 200
+    isPassed = false
+    isSpade = false
+    isntSpade = false
+    randomPoint = love.math.random(500, 1700)
 end
 
 function parallax.update(dt)
     x = x - speed * dt
+
+    if x < -300 then -- pour que la pelle revienne a sa position de depart a droite de l'ecran
+        x = love.graphics.getWidth() + sprite:getWidth() * scale - 200
+        y = love.graphics.getHeight() - sprite:getHeight() * scale - 800
+        isPassed = false -- savoir si la pelle est passé par le point de descente
+        isSpade = false -- savoir si la pelle est en train de se baisser
+        isntSpade = false -- savoir si la pelle est en train de se relever
+        randomPoint = love.math.random(500, 1700) -- point de descente random
+
+    elseif x < randomPoint and isPassed == false then -- pour que la pelle se baisse en fonction d'un point random (randomPoint)
+        if love.math.random(2) == 1 then -- pour que la pelle se baisse suivant un random de 2
+            isSpade = true
+        else
+            isPassed = true
+        end
+    end
+
+    if x < randomPoint - 500 then -- pour que la pelle se releve 500px apres le point de descente
+        isntSpade = true
+    end
+
+    if isSpade == true and isntSpade == false and y < love.graphics.getHeight() - sprite:getHeight() * scale - 480 then -- pour que la pelle se baisse
+        y = y + 500 * dt
+    end
+    if isntSpade == true and y > love.graphics.getHeight() - sprite:getHeight() * scale - 800 then -- pour que la pelle se releve
+        y = y - 500 * dt
+    end
+
 end
 
 function parallax.draw()
-    love.graphics.draw(sprite, x, y)
+    love.graphics.draw(sprite, x, y, 0, scale, scale)
 end
