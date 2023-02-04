9273feaf20ed59adc574afdc567ee2b4e19d8b1a
queng <quentin.brejoin@gmail.com>
2023-02-04 18:19:38 +0100

    feat(parallaxe): add parallax

--- main.lua
+++ main.lua
@@ -1,14 +1,9 @@
 function love.load()
     love.window.setMode(1920, 1080)
-    s_dirt = love.graphics.newImage("assets/dirt.png")
-    s_grass = love.graphics.newImage("assets/grass.png")
 end
 
-function love.update()
-
+function love.update(dt)
 end
 
 function love.draw(dt)
-    love.graphics.draw(s_dirt, 0, 0)
-    love.graphics.draw(s_grass, 0, 0)
 end
--- parallaxe.lua
+++ parallaxe.lua
@@ -1,13 +1,29 @@
 parallaxe = {}
 
-function obstacles.load()
-    s_dirt = love.graphics.newImage("assets/dirt.png")
+local background = love.graphics.newImage("assets/grass.png")
+local dirt = love.graphics.newImage("assets/dirt.png")
+local cloud = love.graphics.newImage("assets/cloud.png")
+
+function parallaxe.load()
+    background_scroll = 0
+    cloud_scroll = 0
 end
 
-function obstacles.update()
-
+function parallaxe.update(dt, speed)
+    background_scroll = background_scroll - speed * dt
+    if background_scroll <= -background:getWidth() then
+        background_scroll = 0
+    end
+    cloud_scroll = cloud_scroll - speed / 2 * dt
+    if cloud_scroll <= -background:getWidth() then
+        cloud_scroll = 0
+    end
 end
 
-function obstacles.draw()
-    love.graphics.draw(s_dirt, 0, 0)
+function parallaxe.draw()
+    love.graphics.draw(dirt, 0, 0)
+    love.graphics.draw(background, background_scroll, 0)
+    love.graphics.draw(background, background_scroll + background:getWidth(), 0)
+    love.graphics.draw(cloud, cloud_scroll, -100, 0, 0.2, 0.2)
+    love.graphics.draw(cloud, cloud_scroll + cloud:getWidth(), -100, 0, 0.2, 0.2)
 end
