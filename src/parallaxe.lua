f8e52431a55fc4a8199340e4b281b924ac76c8a0
queng <quentin.brejoin@gmail.com>
2023-02-05 12:09:32 +0100

    fix parallax tree

--- src/parallaxe.lua
+++ src/parallaxe.lua
@@ -7,11 +7,15 @@ local s_tree = love.graphics.newImage("assets/tree.png")
 
 function parallaxe.load()
     background_scroll = 0
+    tree_scroll = 0
     cloud_scroll = 0
 end
 
 function parallaxe.update(dt, speed)
     background_scroll = background_scroll - speed * dt
+    if tree_scroll > -background:getWidth() then
+        tree_scroll = tree_scroll - speed * dt
+    end
     if background_scroll <= -background:getWidth() then
         background_scroll = 0
     end
@@ -27,5 +31,5 @@ function parallaxe.draw()
     love.graphics.draw(background, background_scroll + background:getWidth(), 0)
     love.graphics.draw(cloud, cloud_scroll, -100, 0, 0.2, 0.2)
     love.graphics.draw(cloud, cloud_scroll + cloud:getWidth(), -100, 0, 0.2, 0.2)
-    love.graphics.draw(s_tree, background_scroll - 300, -750, 0, 3, 3)
+    love.graphics.draw(s_tree, tree_scroll - 300, -750, 0, 3, 3)
 end
